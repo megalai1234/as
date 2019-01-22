2af3e045c3d629764780b488193a3e126b4fc538
megalai1234 <43366156+megalai1234@users.noreply.github.com>
2019-01-22 12:44:54 +0530

    Create bb.c

---
+++ bb.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <conoi.h>
+void  main()
+{
+	int n,i;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{ 
+		printf("Hello World ");
+	}
+	getch ();
+}
